edb521f27dd58c0a5e75cd72cdc324c4d236ee9c
Thomas Rohde <trohde@rocketmail.com>
2020-12-31 13:09:04 -0600

    adding div with p element that includes clickcounter. set clickCounter variable to 0 and added code to increment in addDiv

--- client.js
+++ client.js
@@ -1,8 +1,36 @@
 console.log('Autobots Roll Out');
 
+let clickCounter = 0;
+
 $(document).ready(handleReady);
 
+function handleReady() {
+    console.log('in handleReady');
 
+    //click listener for Generate Button
+    $(document).on('click', '#genButton', addDiv);
+
+}; //end handleReady
+
+function addDiv() {
+    console.log('clicked GENERATE');
+
+    //getting rid of zombie dupes
+    $('#clickCounter').empty();
+    //incrementing clickCounter
+    clickCounter++;
+    console.log(clickCounter);
+
+    //appending div element to DOM
+    $('.element').append(`
+    <div class="redDiv">
+    <p id="clickCounter">CLICK COUNTER: ${clickCounter}</p>
+    `)
+
+    //clearing input
+    $('#clickCounter').val('');
+
+}; //end addDiv
 
 
 
